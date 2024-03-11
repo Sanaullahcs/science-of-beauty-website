8ecd1205fdaf4b9a46f195cd6c75762c29d640eb
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-03-11 12:33:13 +0500

    Pricing API is integrated

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/CustomCSS/ContactUs.css";
 import { Button, Grid, TextField } from "@material-ui/core";
 import CallIcon from "@mui/icons-material/Call";
@@ -6,9 +6,10 @@ import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import XIcon from '@mui/icons-material/X';
+import XIcon from "@mui/icons-material/X";
 import send from "../assets/images/send.png";
 import Plx from "react-plx";
+import POST_CONTACT from "../env/apiConfig";
 
 function ContactUs() {
   const parallaxDataTxt = [
@@ -34,32 +35,9 @@ function ContactUs() {
         },
       ],
     },
-    // {
-    //   start: 4200,
-    //   end: 5200,
-    //   properties: [
-    //     {
-    //       startValue: 1.5,
-    //       endValue: 1,
-    //       property: "scale",
-    //     },
-
-    //   ],
-    // },
-    // {
-    //   start: 4000,
-    //   end: 5200,
-    //   properties: [
-    //     {
-    //       startValue: 0,
-    //       endValue: 1,
-    //       property: "opacity",
-    //     },
-    //   ],
-    // },
   ];
-  const [activeButton, setActiveButton] = useState('phone');
-  const [activeSocialButton, setActiveSocialButton] = useState('instagram');
+  const [activeButton, setActiveButton] = useState("phone");
+  const [activeSocialButton, setActiveSocialButton] = useState("instagram");
   const [activeCategories, setActiveCategories] = useState(new Set());
   const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName);
@@ -80,7 +58,7 @@ function ContactUs() {
   };
   return (
     <>
-      <div style={{ padding: '4 % 0', overflow: 'hidden', padding: '2% 0' }}>
+      <div style={{ padding: "4 % 0", overflow: "hidden", padding: "2% 0" }}>
         <Plx parallaxData={parallaxDataTxt}>
           <div className="contact-card">
             <Grid container>
@@ -92,21 +70,27 @@ function ContactUs() {
                   </p>
                   <div className="btn-wrapper">
                     <Button
-                      className={`contact-buttons ${activeButton === "email" ? "active-contact-btn" : ""}`}
+                      className={`contact-buttons ${
+                        activeButton === "email" ? "active-contact-btn" : ""
+                      }`}
                       onClick={() => handleButtonClick("email")}
                     >
                       <MailOutlineIcon style={{ color: "#9d4a43" }} />
                       <span>Info@thescienceofbeauty.co</span>
                     </Button>
                     <Button
-                      className={`contact-buttons ${activeButton === "phone" ? "active-contact-btn" : ""}`}
+                      className={`contact-buttons ${
+                        activeButton === "phone" ? "active-contact-btn" : ""
+                      }`}
                       onClick={() => handleButtonClick("phone")}
                     >
                       <CallIcon style={{ color: "#9d4a43" }} />
                       <span>+123 456 789</span>
                     </Button>
                     <Button
-                      className={`contact-buttons ${activeButton === "location" ? "active-contact-btn" : ""}`}
+                      className={`contact-buttons ${
+                        activeButton === "location" ? "active-contact-btn" : ""
+                      }`}
                       onClick={() => handleButtonClick("location")}
                     >
                       <LocationOnIcon style={{ color: "#9d4a43" }} />
@@ -115,17 +99,31 @@ function ContactUs() {
                   </div>
                   <div>
                     <div className="social-row">
-                      <a href="https://www.facebook.com/Goddesshairxo" target="_blank">
+                      <a
+                        href="https://www.facebook.com/Goddesshairxo"
+                        target="_blank"
+                      >
                         <div
-                          className={`social-icons-span ${activeSocialButton === "facebook" ? "active-social-btn" : ""}`}
+                          className={`social-icons-span ${
+                            activeSocialButton === "facebook"
+                              ? "active-social-btn"
+                              : ""
+                          }`}
                           onClick={() => handleSocialButtonClick("facebook")}
                         >
                           <FacebookIcon style={{ color: "#eee" }} />
                         </div>
                       </a>
-                      <a href="https://www.instagram.com/thescienceofbeautydallas/" target="_blank">
+                      <a
+                        href="https://www.instagram.com/thescienceofbeautydallas/"
+                        target="_blank"
+                      >
                         <div
-                          className={`social-icons-span ${activeSocialButton === "instagram" ? "active-social-btn" : ""}`}
+                          className={`social-icons-span ${
+                            activeSocialButton === "instagram"
+                              ? "active-social-btn"
+                              : ""
+                          }`}
                           onClick={() => handleSocialButtonClick("instagram")}
                         >
                           <InstagramIcon style={{ color: "#eee" }} />
@@ -133,7 +131,11 @@ function ContactUs() {
                       </a>
                       <a href="#" target="_blank">
                         <div
-                          className={`social-icons-span ${activeSocialButton === "twitter" ? "active-social-btn" : ""}`}
+                          className={`social-icons-span ${
+                            activeSocialButton === "twitter"
+                              ? "active-social-btn"
+                              : ""
+                          }`}
                           onClick={() => handleSocialButtonClick("twitter")}
                         >
                           <XIcon style={{ color: "#eee" }} />
@@ -145,41 +147,61 @@ function ContactUs() {
               </Grid>
               <Grid items lg={6} md={12}>
                 <div className="additional-div">
-                  {/* <p className="contact-interested-heading">
+                  <p className="contact-interested-heading">
                     I’m interested in...
-                  </p> */}
-                  {/* <div className="btn-wrap">
+                  </p>
+                  <div className="btn-wrap">
                     <Button
-                      className={`category-btn ${activeCategories.has("Makeup Artist") ? "active-category-btn" : ""}`}
+                      className={`category-btn ${
+                        activeCategories.has("Makeup Artist")
+                          ? "active-category-btn"
+                          : ""
+                      }`}
                       onClick={() => handleCategoryButtonClick("Makeup Artist")}
                     >
                       Makeup Artist
                     </Button>
                     <Button
-                      className={`category-btn ${activeCategories.has("Barbersalon") ? "active-category-btn" : ""}`}
+                      className={`category-btn ${
+                        activeCategories.has("Barbersalon")
+                          ? "active-category-btn"
+                          : ""
+                      }`}
                       onClick={() => handleCategoryButtonClick("Barbersalon")}
                     >
                       Barbersalon
                     </Button>
                     <Button
-                      className={`category-btn ${activeCategories.has("Frisorsalon") ? "active-category-btn" : ""}`}
+                      className={`category-btn ${
+                        activeCategories.has("Frisorsalon")
+                          ? "active-category-btn"
+                          : ""
+                      }`}
                       onClick={() => handleCategoryButtonClick("Frisorsalon")}
                     >
                       Frisorsalon
                     </Button>
                     <Button
-                      className={`category-btn ${activeCategories.has("Massageclinic") ? "active-category-btn" : ""}`}
+                      className={`category-btn ${
+                        activeCategories.has("Massageclinic")
+                          ? "active-category-btn"
+                          : ""
+                      }`}
                       onClick={() => handleCategoryButtonClick("Massageclinic")}
                     >
                       Massageclinic
                     </Button>
                     <Button
-                      className={`category-btn other-contact-btn ${activeCategories.has("Other") ? "active-category-btn" : ""}`}
+                      className={`category-btn other-contact-btn ${
+                        activeCategories.has("Other")
+                          ? "active-category-btn"
+                          : ""
+                      }`}
                       onClick={() => handleCategoryButtonClick("Other")}
                     >
                       Other
                     </Button>
-                  </div> */}
+                  </div>
                   <div className="contact-textField-div">
                     <TextField
                       className="contact-form-fields"
